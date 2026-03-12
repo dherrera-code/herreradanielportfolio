a93834dcdf4e3626074a6f64d2225ea8652b0f15
dherrera-code <dherr929@gmail.com>
2026-03-12 13:44:40 -0700

    installed aurora background but removed implementation!

--- components/TopNavbar.tsx
+++ components/TopNavbar.tsx
@@ -6,15 +6,15 @@ import React from 'react'
 const TopNavbar = () => {
     return (
         <div>
-            <Navbar fluid className='bg-black text-white py-10'>
+            <Navbar fluid className='bg-black text-white py-5'>
                 <a href="#Top"><h1 className='ps-5 text-3xl'>Daniel Herrera</h1></a>
                 <NavbarToggle />
                 <NavbarCollapse className='place-self-center'>
                     <NavbarLink className='me-6 self-center text-white text-xl pt-100' href="#">
                         About
                     </NavbarLink>
-                    <NavbarLink className='me-6 self-center text-white text-xl' href="#">Services</NavbarLink>
-                    <NavbarLink className='me-6 self-center text-white text-xl' href="#">Pricing</NavbarLink>
+                    <NavbarLink className='me-6 self-center text-white text-xl' href="#">Skills</NavbarLink>
+                    <NavbarLink className='me-6 self-center text-white text-xl' href="#">Projects</NavbarLink>
                     <NavbarLink className='me-6 self-center text-white' href="#"><Button className='text-xl'>Contact</Button></NavbarLink>
                 </NavbarCollapse>
             </Navbar>
