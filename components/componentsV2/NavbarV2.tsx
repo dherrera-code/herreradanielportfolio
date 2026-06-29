3148736987254296b4c071df1b197b4bb5986357
Daniel <dherr929@gmail.com>
2026-06-29 10:11:57 -0700

    Updated resume

--- components/componentsV2/NavbarV2.tsx
+++ components/componentsV2/NavbarV2.tsx
@@ -21,7 +21,7 @@ const NavbarV2 = () => {
                     <NavbarLink className={`me-6 md:mt-1.5 text-gray-800 text-lg`} href="/#project">Projects</NavbarLink>
                     <NavbarLink className='me-6 md:mt-1.5 text-gray-800 text-lg' href="/#stack">Stack</NavbarLink>
                     <NavbarLink className='me-6 md:mt-1.5 text-gray-800 text-lg' href="/#contact">Contact</NavbarLink>
-                    <a download='/HerreraDResumeATSv4.pdf' href='./HerreraDResumeATSv4.pdf' className='bg-[#1F108E] px-5 py-2 rounded-xl text-center text-white text-lg block md:hidden'>Resume</a>
+                    <a download='/HerreraDResumeATS.pdf' href='./HerreraDResumeATS.pdf' className='bg-[#1F108E] px-5 py-2 rounded-xl text-center text-white text-lg block md:hidden'>Resume</a>
                 </NavbarCollapse>
                 <div className='md:block hidden'>
                     <div className='flex gap-5 '>
@@ -31,7 +31,7 @@ const NavbarV2 = () => {
                         <a target='_blank' href="https://www.linkedin.com/in/daniel-bravo-herrera/">
                             <img title='Redirect to my LinkedIn' className='w-auto h-10' src="/assets/LinkedIn.png" alt="LinkedIn Logo" />
                         </a>
-                        <a title='Click to download resume!' download='/HerreraDResumeATSv4.pdf' href='./HerreraDResumeATSv4.pdf' className='bg-[#1F108E] px-5 py-2 rounded-xl text-white text-lg'>Resume</a>
+                        <a title='Click to download resume!' download='/HerreraDResumeATS.pdf' href='./HerreraDResumeATS.pdf' className='bg-[#1F108E] px-5 py-2 rounded-xl text-white text-lg'>Resume</a>
                     </div>
                 </div>
             </Navbar>
